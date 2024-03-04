ae3ca786a4473ad5e253b45c496590023cf6d30d
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-03-04 13:05:45 +0100

    Fixing calculation issues + Add menu

--- riven_roll.cpp
+++ riven_roll.cpp
@@ -1,24 +1,60 @@
 
 #include <iostream>
+#include <conio.h>
+#include "riven_roll.h"
+#include <cstdlib>
 
 int main() {
-    int input;
-    int kuvaCost = 900;
+    bool running = true;
+    while (running) {
+        system("cls");
+        int input;
+        printf("=========================\n");
+        printf("| Riven Roll Calculator |\n");
+        printf("=========================\n");
+        printf("Choose an Option:\n");
+        printf("1. Calculate Kuva Cost\n");
+        printf("2. Exit\n");
+        printf("=========================\n");
+        std::cout << "Enter you choice: ";
+        std::cin >> input;
+        system("cls");
+
+        switch (input)
+        {
+        case 1:
+            calculCost();
+            break;
+        
+        default:
+            running = false;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void calculCost(){
     int result = 0;
+    int rollsNb;
     std::cout << "Enter the number of rolls: ";
-    std::cin >> input;
+    std::cin >> rollsNb;
 
     printf("==============================\n");
     int i = 1;
-    while (i <= input) {
-        result += kuvaCost;
-        if (i > 1) {
-            result += 100*(i-1);
-        }
+    while (i <= rollsNb && i < 10) {
+        result += ROLL_COSTS[i-1];
+        printf("Roll %d: %d\n", i, ROLL_COSTS[i-1]);
         i++;
+    }
+    if (rollsNb >= 10) {
+        result += (rollsNb-9)*ROLL_COSTS[9];
+        printf("Roll %s: %d\n", "10+", ROLL_COSTS[9]);
     }
     printf("Total Kuva cost: %d\n", result);
     printf("==============================\n");
-
-    return 0;
+    printf("Press any key to continue...");
+    _getch();
+    return;
 }
---
+++ riven_roll.h
@@ -0,0 +1,10 @@
+const int DEFAULT_KUVA_COST = 900;
+const int ROLL_COSTS[] = {900, 1000, 1200, 1400, 1700, 2000, 2350, 2750, 3150, 3500};
+
+/*
+*  @brief Calculate the cost of a riven roll
+*  @param void
+*  @return void
+*  @note This function will prompt the user to enter the number of rolls and will calculate the cost of the rolls
+*/
+void calculCost();
